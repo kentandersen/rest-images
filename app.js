9769d7b9c64a73184d95269ed4a8a1ad19c906ec
Kent Andersen <kentareandersen@gmail.com>
2014-08-14 21:43:00 +0200

    enabled cors

--- app.js
+++ app.js
@@ -7,7 +7,14 @@ var topics = require("./topics");
 
 var app = express();
 
-// jsonp api
+// Enable CORS
+app.all('*', function(req, res, next) {
+    res.header("Access-Control-Allow-Headers", "X-Requested-With");
+    res.header("Access-Control-Allow-Origin", "*");
+    next();
+});
+
+// json api
 app.get('/', function(req, res) {
 
     var topic = topics.getRandom();
