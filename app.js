30b3157528b98490d907ae12a5869f94f214a534
Kent Andersen <kentareandersen@gmail.com>
2014-08-14 21:25:39 +0200

    removed console.log

--- app.js
+++ app.js
@@ -11,8 +11,8 @@ app.use("/images", express.static(path.join(__dirname, "images")));
 
 // jsonp api
 app.get('/', function(req, res) {
+
     var topic = topics.getRandom();
-    console.log(topic)
     var promise = fetch.image(topic);
 
     promise.then(function(promise) {
