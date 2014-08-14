98f653d808f07fc23643eca49d72fb50cd47cbbf
Kent Andersen <kentareandersen@gmail.com>
2014-08-14 21:24:12 +0200

    Changed to use google as base

--- app.js
+++ app.js
@@ -2,25 +2,36 @@
 var path = require("path");
 var glob = require("glob");
 var express = require("express");
+var fetch = require("./fetch");
+var topics = require("./topics");
 
 var app = express();
 
-glob(path.join("images", "*.+(jpg|png)"), function(error, matches) {
+app.use("/images", express.static(path.join(__dirname, "images")));
 
-    app.use("/images", express.static(path.join(__dirname, "images")));
+// jsonp api
+app.get('/', function(req, res) {
+    var topic = topics.getRandom();
+    console.log(topic)
+    var promise = fetch.image(topic);
 
-    // jsonp api
-    app.get('/', function(req, res) {
-        var baseUrl = "http://" + req.headers.host + "/";
-        res.jsonp({
-            images: matches.map(function(image) {
-                return baseUrl + image;
-            })
-        });
+    promise.then(function(promise) {
+        res.json(promise);
     });
 
+});
+
+app.get('/:query', function(req, res) {
+
+    var promise = fetch.image(req.params.query);
+
+    promise.then(function(promise) {
+        res.json(promise);
+    });
 
 });
+
+
 // if on port is set, use port.
 var port = process.env.PORT || 1339;
 app.listen(port);
